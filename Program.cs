c81ad7dec5a3e346ec7e63387d9453ebdcdf17f6
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-15 16:21:39 +0530

    Welcome to Line Computation Problems

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using LineComparisionComputationProblem;
+
+namespace LineComparisionProblems
+{
+    class LineComparisionPrograms
+    {
+        public static void Main(String[] args)
+        {
+            //WelcomeToLineComputationProblem.WelcometoLineComputation();
+            LineComparisionProblem_UseCase1.LineComparisionProblemUC1();
+        }
+    }
+}
